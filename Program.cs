2455db62e59c3ff2919aee10514701f6641b3bbf
Akil Turner-Richards <ki1orich@gmail.com>
2025-05-06 15:25:56 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("How many numbers to print: ");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintNaturalNumbers_TailRecursive(n);
+
+void PrintNaturalNumbers_TailRecursive(int n)
+{
+    if (n > 0)
+    {
+        Console.Write($"{n} ");
+        PrintNaturalNumbers_TailRecursive(n - 1);
+    }
+}
